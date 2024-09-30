f9fdc5cf66fb3fd924558495a92a586c3d39d4aa
Kelvin Ung <Kelvin@Kelvins-MacBook-Pro.local>
2024-09-30 13:55:31 +0100

    port update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use("/api/artworks", artworkRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
